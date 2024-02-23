997a9c08dc974e28241db909a2152e54b84c05be
nhnguyen <nhnguyen@axon.com>
2024-02-23 15:20:53 +0700

    Update - replace prefix bundleId

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./setup.sh com.nyon.WebDriverAgentRunner appium-ios-test
+# ./setup.sh com.nyon appium-ios-test
 
 BUNDLE_ID=$1
 DOCKER_TAG=$2
@@ -9,10 +9,21 @@ echo "Installing USB multiplexing daemon..."
 apt install usbmuxd
 
 # Build go-ios tool
-echo "Modifying default WDA bundleID to $BUNDLE_ID"
-# TODO: Testing sed command
-# sed -i "s/com.facebook.WebDriverAgentRunner/$BUNDLE_ID/g" go-ios/main.go
-./build_ios_tools.sh
+echo "Modifying default WDA bundleID 'com.facebook' to $BUNDLE_ID"
+directory="go-ios"
+
+# Define the string to search for and the string to replace it with
+search_string="com.facebook"
+
+# Use grep to find files containing the search string
+files_with_string=$(grep -rl --include="*.go" "$search_string" "$directory")
+
+# Loop through the files found and use sed to replace the string
+for file in $files_with_string; do
+    sed -i '' -e "s/$search_string/$BUNDLE_ID/g" "$file"
+done
+
+# ./build_ios_tools.sh
 
 echo "Building docker..."
 docker build -t $DOCKER_TAG .
